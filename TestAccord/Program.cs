cccffcbcaf31e197d5f0b45bf1b8d2c15a7b7d03
denis <tran@clearvisionboxes.com>
2016-03-28 21:57:45 -0700

    first commit of project to test SVM on XOR

---
+++ TestAccord/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.Controls;
+using Accord.MachineLearning.VectorMachines;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Math;
+using Accord.Statistics.Kernels;
+
+namespace TestAccord
+{
+    class Program
+    {
+        [MTAThread]
+        static void Main(string[] args)
+        {
+            double[][] inputs ={
+                                    new double[]{0,0},
+                                    new double[]{1,0},
+                                    new double []{0,1},
+                                    new double[]{1,1},
+                               };
+
+            int[] outputs = {
+                                -1,
+                                +1,
+                                +1,
+                                -1,
+                            };
+
+            double[][] inputs2 ={
+                                    new double[]{0,0},
+                                    new double[]{0,1},
+                                    new double []{1,0},
+                                    new double[]{1,1},
+                               };
+
+            var ksvm = new KernelSupportVectorMachine(new Gaussian(), 2);
+            var smo = new SequentialMinimalOptimization(machine: ksvm, inputs: inputs, outputs: outputs)
+            {
+                Complexity = 100
+            };
+
+            double error = smo.Run();
+
+            Console.WriteLine("error:" + error);
+
+            // Show results on screen 
+            
+            ScatterplotBox.Show("Training data", inputs, outputs);
+
+            ScatterplotBox.Show("SVM results", inputs2,
+                inputs2.Apply(p => System.Math.Sign(ksvm.Compute(p))));
+
+            Console.ReadKey(); 
+        }
+    }
+}
